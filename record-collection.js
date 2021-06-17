b8a4f5db4ee806d73b87eb45557334feb4d3c895
JasonJF <jasonf@reutech.co.za>
2021-06-17 20:09:44 +0200

    first commit

---
+++ record-collection.js
@@ -0,0 +1,46 @@
+// Setup
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    if (prop != "tracks" && value != ""){
+        records[id][prop] = value;
+        }
+    else if (prop == "tracks"){
+        if(!(records[id].hasOwnProperty("tracks"))){
+            records[id].tracks = [value];
+        }
+        else if(value != ""){
+            records[id].tracks.push(value);
+        }
+        else if(value == ""){
+           delete records[id][prop];
+        }
+    }
+    else if(prop != "tracks"){
+        delete records[id][prop];
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+updateRecords(recordCollection, 2548, "tracks", "")
